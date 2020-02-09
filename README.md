f0d0b56873d68e92f53cfa5d91a48c0e80f3cdd2
nyaundi brian <danleyb2@gmail.com>
2020-02-09 11:12:02 +0300

    initial commit from template

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
-# web-component
+# rich-text-editor
 
-Web Component Starter
+Rich Text Editor
 
-![Node.js Package](https://github.com/danleyb2/web-component/workflows/Node.js%20Package/badge.svg)
+![Node.js Package](https://github.com/danleyb2/rich-text-editor/workflows/Node.js%20Package/badge.svg)
 
-A starter project for building re-usable web components using lit-element 
+A Rich Text Editor Web Component 
 
 ## Features
-- LitElement for a declarative UI, automatic updates when properties change.
-- Follows Web Components standards, works with any framework
-- Building with bundling and transpilation using polymer build 
-- Testing with Web-Component-Tester
+- Tiny
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { LitElement,css, html } from 'lit-element';
+import {init} from './src/pell';
 
-class WebComponent extends LitElement {
+class RichTextEditor extends LitElement {
   static get properties() {
     return { name: { type: String } };
   }
@@ -12,11 +13,32 @@ class WebComponent extends LitElement {
 
     static get styles() {
         return css`
-        
-        #content{
-        color:red;
-        
-        }
+      .pell {
+    border: 1px solid rgba(10, 10, 10, 0.1);
+    box-sizing: border-box; }
+
+.pell-content {
+    box-sizing: border-box;
+    height: 300px;
+    outline: 0;
+    overflow-y: auto;
+    padding: 10px; }
+
+.pell-actionbar {
+    background-color: #FFF;
+    border-bottom: 1px solid rgba(10, 10, 10, 0.1); }
+
+.pell-button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    height: 30px;
+    outline: 0;
+    width: 30px;
+    vertical-align: bottom; }
+
+.pell-button-selected {
+    background-color: #F0F0F0; }
 
     `;
     }
@@ -24,17 +46,41 @@ class WebComponent extends LitElement {
 
   render() {
     return html`
-        Hello <span id="content">${this.name}</span>!
+        <div id="pell"></div>
 `;
   }
 
     firstUpdated() {
+        const self = this;
+        // Initialize pell on an HTMLElement
+        init({
+            // <HTMLElement>, required
+            element: this.shadowRoot.getElementById('pell'),
 
+            // <Function>, required
+            // Use the output html, triggered by element's `oninput` event
+            onChange: html => self.value = html,
+
+            // <string>, optional, default = 'div'
+            // Instructs the editor which element to inject via the return key
+            defaultParagraphSeparator: 'div',
+
+            // <boolean>, optional, default = false
+            // Outputs <span style="font-weight: bold;"></span> instead of <b></b>
+            styleWithCSS: false,
+
+
+            // classes<Array[string]> (optional)
+            // Choose your custom class names
+            classes: {
+                actionbar: 'pell-actionbar',
+                button: 'pell-button',
+                content: 'pell-content',
+                selected: 'pell-button-selected',
+            },
+        });
     }
-
-
-
 
 }
 
-customElements.define('web-component', WebComponent);
+customElements.define('rich-text-editor', RichTextEditor);
